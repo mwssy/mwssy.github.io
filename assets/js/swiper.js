a3b7382b4743da3c29705fd72634e8ea477e355f
mwssy <mokmail070@mokwon.ac.kr>
2023-06-16 23:20:22 +0900

    수정중

---
+++ assets/js/swiper.js
@@ -0,0 +1,10 @@
+new Swiper('.page-section .swiper .swiper-container', {
+    direction: 'horizontal',
+    autoplay: {
+    delay: 5000
+    },
+    loop: false,
+    spaceBetween: 10,
+    slidesPerView: 3,
+    centeredSlides: true,
+});
